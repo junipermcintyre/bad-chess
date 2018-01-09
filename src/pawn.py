9f6526a49962ebb0a6074546463d48cd080bcd4f
junipermcintyre <junipermcintyre@gmail.com>
2018-01-09 13:50:31 -0500

    Pawn move 2 spaces on first move

--- src/pawn.py
+++ src/pawn.py
@@ -2,6 +2,7 @@ from .piece import Piece
 # Pawn chess piece
 class Pawn(Piece):
     name = "pawn"
+    hasMoved = False        # Pawns can move two spaces for their first move
 
     """a and b are coords"""
     def move(self, a, b, defender, map):
@@ -10,18 +11,25 @@ class Pawn(Piece):
 
         if (defender is not None):
             if defender.team is not self.team:
-                if b['x'] + 1 == a['x'] or b['x'] - 1 == a['x']: # Diagnol
+                if b['x'] + 1 is a['x'] or b['x'] - 1 is a['x']: # Diagnol
                     xCheck = True
         else:
-            if b['x'] == a['x']: # straight
+            if b['x'] is a['x']: # straight
                 xCheck = True
 
         if self.team is "white": #Go down (white on top) 0->7, 0v7
-            if b['y'] == a['y'] + 1:
+            if b['y'] is a['y'] + 1:
+                yCheck = True
+            elif b['y'] is a['y'] + 2 and not self.hasMoved:    # First move can be two
                 yCheck = True
         else:
-            if b['y'] == a['y'] - 1:
+            if b['y'] is a['y'] - 1:
                 yCheck = True
+            elif b['y'] is a['y'] - 2 and not self.hasMoved:    # First move can be two
+                yCheck = True
+
+        if xCheck and yCheck:
+            self.hasMoved = True
 
         return xCheck and yCheck
 
