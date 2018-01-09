4a21ad63c93f5fce166491dd9b7fc8d5b9659d68
junipermcintyre <junipermcintyre@gmail.com>
2018-01-09 12:46:03 -0500

    Blind commit of king, didn't test lol

--- src/king.py
+++ src/king.py
@@ -5,7 +5,29 @@ class King(Piece):
 
     """a and b are coords"""
     def move(self, a, b, defender, map):
-        return False
+        # Kings can move... one space. Any direction. Thus...
+        # The maximum difference in xs must be one
+        # The maximum difference in ys must be one
+        # The maximum total difference between the two must be two
+        # The minimum total difference between the two must be one (something must've moved)
+
+        diffX = abs(a['x'] - b['x'])
+        diffY = abs(a['y'] - b['y'])
+
+        if diffX > 1 or diffY > 1:  # Can't move more than 1 in x/y
+            return False
+
+        if diffX + diffY > 2 or diffX + diffY < 1: # in total, can't move more than 2 or less than 1
+            return False
+
+        # Check if the space if friendly (can't move)
+        finalSpace = map.getPiece(b)
+        if finalSpace is not None:
+            if finalSpace.team is self.team:
+                return False
+
+
+        return True
 
 
     def getSymbol(self):
